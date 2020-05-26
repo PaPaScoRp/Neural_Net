295124023b4494b7500ec3a0b5c1703441259753
PaPa ScoRp <scorpionfire4@gmail.com>
2020-05-26 02:22:25 -0400

    Updated Spiral_Data

--- Project1/Generate_Data.h
+++ Project1/Generate_Data.h
@@ -3,26 +3,11 @@
 struct Spiral_Data {
 	//X = inputs. A 2D array
 	//Y = ? 1D array
-	Eigen::Array2Xd X;
-	Eigen::ArrayXd y;
+	Eigen::MatrixXd X;
+	Eigen::VectorXd y;
 	Spiral_Data(int, int);
 };
-/*
-def spiral_data(points, classes):
-	X = np.zeros((points*classes, 2))
 
-	y = np.zeros(points*classes, dtype='uint8')
-	for class_number in range(classes):
-		ix = range(points*class_number, points*(class_number+1))
-		r = np.linspace(0.0, 1, points)  # radius
-		t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
-		print (r)
-		X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-		y[ix] = class_number
-	return X, y
-
-print(spiral_data(3, 100))
-*/
 /*
 
 points = 3
@@ -43,9 +28,24 @@ ix =range ( 0,3)
 	}
 	*/
 
+	/*
+	def spiral_data(points, classes):
+		X = np.zeros((points*classes, 2))
+
+		y = np.zeros(points*classes, dtype='uint8')
+		for class_number in range(classes):
+			ix = range(points*class_number, points*(class_number+1))
+			r = np.linspace(0.0, 1, points)  # radius
+			t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+			X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+			y[ix] = class_number
+		return X, y
+
+	print(spiral_data(3, 100))
+	*/
 
 Spiral_Data::Spiral_Data(int points = 0, int const classes) {
-	X = Eigen::ArrayXd::Zero((points * classes), 2);
+	X = Eigen::ArrayXXd::Zero((points * classes), 2);
 	y = Eigen::ArrayXd::Zero(points * classes);
 	vector<int> ix;
 	for(int class_number = 0; class_number < classes; class_number++) {
@@ -53,10 +53,11 @@ Spiral_Data::Spiral_Data(int points = 0, int const classes) {
 		for(int i = points * class_number; i < points * (class_number + 1); i++) {
 
 			ix.push_back(i);
+			Eigen::VectorXd r = Eigen::VectorXd::LinSpaced(points, 0.0, 1).transpose(); //radius
+			Eigen::VectorXd t = Eigen::VectorXd::LinSpaced(points, class_number * 4, (class_number + 1) * 4).transpose() + Eigen::VectorXd::Random(points).transpose()*0.2;
+			for (int j = 0; j < ix.size(); j++)
 
-
-
-
+				X << r * (t * 2.5).sin(), r* (t * 2.5).cos();
 
 		}
 	}
--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -4,6 +4,7 @@
 #include "Activation_ReLu.h"
 #include "Timer.h"
 #include "Generate_Data.h"
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -12,30 +13,27 @@ int main() {
 
 
 
-		//Eigen::ArrayXd a(4), b(4); 
-		//a(0) = 1; a(1) = 2; a(2) = 3; a(3) = 5; 
-		//b(0) = 2; b(1) = 0; b(2) = 4; b(3) = 0; 
-		//cout << a.max(b) << endl;					
-		int b = 10;
-		for (int i = 0; i <= b; i++) {
-			cout << i << endl;
-		}
-
-
-		Eigen::MatrixXd inputs(3, 4);
-		inputs << 1, 2, 3, 2.5,
-			2.0, 5.0, -1.0, 2.0,
-			-1.5, 2.7, 3.3, -0.8;
-		Layer_Dense layer1(4, 5);
-		Layer_Dense layer2(5, 2);
-		Activation_ReLu activation;
-
-		cout << layer2.forward(activation.forward(layer1.forward(inputs))) << "\n\n\n";
-
+		
+		 
+		 
+			
+		
+		
+		
+		
+		
+		
+		//Eigen::MatrixXd inputs(3, 4);
+		//inputs << 1, 2, 3, 2.5,
+		//	2.0, 5.0, -1.0, 2.0,
+		//	-1.5, 2.7, 3.3, -0.8;
+		//Layer_Dense layer1(4, 5);
+		//Layer_Dense layer2(5, 2);
+		//Activation_ReLu activation;
+		//
+		//cout << layer2.forward(activation.forward(layer1.forward(inputs))) << "\n\n\n";
 
 	}
-
-
 
 }
 
